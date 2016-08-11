2cad1348cb0a08f43ade485a9bdafadbd5c85d2b
Tom <thomas.powell263@gmail.com>
2016-08-11 12:29:29 +0100

    Dialog class structure implemented. Citizen list dialog now opens, no content yet

---
+++ city_game/Dialogs/base_dialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace city_game
+{
+    abstract class base_dialog
+    {
+
+        public static SpriteFont font;
+
+        public static Texture2D tex_bkg;
+        public static Texture2D tex_topbar;
+        public static Texture2D tex_exit_button;
+
+        protected int x_start = 400;
+        protected int y_start = 200;
+        protected int width = 200;
+        protected int height = 400;
+
+        public abstract void Update();
+        public abstract void Draw(SpriteBatch spriteBatch);
+
+    }
+}
---
+++ city_game/Dialogs/citizen_list_dialog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+
+namespace city_game
+{
+    class citizen_list_dialog : base_dialog
+    {
+
+        private List<citizen> citizen_list;
+        private bool close = false;
+
+        public citizen_list_dialog(List<citizen> citizens)
+        {
+            citizen_list = citizens;
+            Debug.WriteLine("Dialog constructor");
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Draw(tex_bkg, new Rectangle(x_start, y_start, width+200, height+100), Color.White);
+            //Debug.WriteLine("Drawing dialog");
+
+        }
+        public bool close_dialog()
+        {
+            return close;
+        }
+
+    }
+}
